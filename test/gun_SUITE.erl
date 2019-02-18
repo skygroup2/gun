ddc7979f904cb71e7ade19176321d4a62b6dc87a
Mr Robot <tr.hoan00@gmail.com>
2019-02-18 11:46:37 +0700

    merge TC from master

--- test/gun_SUITE.erl
+++ test/gun_SUITE.erl
@@ -1,4 +1,4 @@
-%% Copyright (c) 2017-2018, Loïc Hoguin <essen@ninenines.eu>
+%% Copyright (c) 2017-2019, Loïc Hoguin <essen@ninenines.eu>
 %%
 %% Permission to use, copy, modify, and/or distribute this software for any
 %% purpose with or without fee is hereby granted, provided that the above
@@ -18,11 +18,39 @@
 
 -import(ct_helper, [doc/1]).
 -import(ct_helper, [name/0]).
+-import(gun_test, [init_origin/2]).
+-import(gun_test, [init_origin/3]).
+-import(gun_test, [receive_from/1]).
+-import(gun_test, [receive_all_from/2]).
 
 all() ->
 	ct_helper:all(?MODULE).
 
 %% Tests.
+
+atom_header_name(_) ->
+	doc("Header names may be given as atom."),
+	{ok, OriginPid, OriginPort} = init_origin(tcp, http),
+	{ok, Pid} = gun:open("localhost", OriginPort),
+	{ok, http} = gun:await_up(Pid),
+	_ = gun:get(Pid, "/", [
+		{'User-Agent', "Gun/atom-headers"}
+	]),
+	Data = receive_from(OriginPid),
+	Lines = binary:split(Data, <<"\r\n">>, [global]),
+	[<<"user-agent: Gun/atom-headers">>] = [L || <<"user-agent: ", _/bits>> = L <- Lines],
+	gun:close(Pid).
+
+atom_hostname(_) ->
+	doc("Hostnames may be given as atom."),
+	{ok, OriginPid, OriginPort} = init_origin(tcp, http),
+	{ok, Pid} = gun:open('localhost', OriginPort),
+	{ok, http} = gun:await_up(Pid),
+	_ = gun:get(Pid, "/"),
+	Data = receive_from(OriginPid),
+	Lines = binary:split(Data, <<"\r\n">>, [global]),
+	[<<"host: localhost:", _/bits>>] = [L || <<"host: ", _/bits>> = L <- Lines],
+	gun:close(Pid).
 
 connect_timeout(_) ->
 	doc("Ensure an integer value for connect_timeout is accepted."),
@@ -53,7 +81,8 @@ detect_owner_gone(_) ->
 	spawn(fun() ->
 		{ok, ConnPid} = gun:open("localhost", Port),
 		Self ! {conn, ConnPid},
-		gun:await_up(ConnPid)
+		gun:await_up(ConnPid),
+		timer:sleep(100)
 	end),
 	{ok, _} = gen_tcp:accept(ListenSocket, 5000),
 	Pid = receive
@@ -106,16 +135,41 @@ detect_owner_gone_ws(_) ->
 	end,
 	cowboy:stop_listener(Name).
 
-shutdown_reason(_) ->
-	doc("The last connection failure must be propagated."),
-	{ok, Pid} = gun:open("localhost", 12345, #{retry => 0}),
-	Ref = monitor(process, Pid),
-	receive
-		{'DOWN', Ref, process, Pid, {shutdown, econnrefused}} ->
-			ok
-	after 200 ->
-		error(timeout)
-	end.
+ignore_empty_data_http(_) ->
+	doc("When gun:data/4 is called with nofin and empty data, it must be ignored."),
+	{ok, OriginPid, OriginPort} = init_origin(tcp, http),
+	{ok, Pid} = gun:open("localhost", OriginPort),
+	{ok, http} = gun:await_up(Pid),
+	Ref = gun:put(Pid, "/", []),
+	gun:data(Pid, Ref, nofin, "hello "),
+	gun:data(Pid, Ref, nofin, ["", <<>>]),
+	gun:data(Pid, Ref, fin, "world!"),
+	Data = receive_all_from(OriginPid, 500),
+	Lines = binary:split(Data, <<"\r\n">>, [global]),
+	Zero = [Z || <<"0">> = Z <- Lines],
+	1 = length(Zero),
+	gun:close(Pid).
+
+ignore_empty_data_http2(_) ->
+	doc("When gun:data/4 is called with nofin and empty data, it must be ignored."),
+	{ok, OriginPid, OriginPort} = init_origin(tcp, http2),
+	{ok, Pid} = gun:open("localhost", OriginPort, #{protocols => [http2]}),
+	{ok, http2} = gun:await_up(Pid),
+	timer:sleep(100), %% Give enough time for the handshake to fully complete.
+	Ref = gun:put(Pid, "/", []),
+	gun:data(Pid, Ref, nofin, "hello "),
+	gun:data(Pid, Ref, nofin, ["", <<>>]),
+	gun:data(Pid, Ref, fin, "world!"),
+	Data = receive_all_from(OriginPid, 500),
+	<<
+		%% HEADERS frame.
+		Len1:24, 1, _:40, _:Len1/unit:8,
+		%% First DATA frame.
+		6:24, 0, _:7, 0:1, _:32, "hello ",
+		%% Second and final DATA frame.
+		6:24, 0, _:7, 1:1, _:32, "world!"
+	>> = Data,
+	gun:close(Pid).
 
 info(_) ->
 	doc("Get info from the Gun connection."),
@@ -124,7 +178,7 @@ info(_) ->
 	{ok, Pid} = gun:open("localhost", Port),
 	{ok, _} = gen_tcp:accept(ListenSocket, 5000),
 	#{sock_ip := _, sock_port := _} = gun:info(Pid),
-	ok.
+	gun:close(Pid).
 
 keepalive_infinity(_) ->
 	doc("Ensure infinity for keepalive is accepted by all protocols."),
@@ -139,6 +193,58 @@ keepalive_infinity(_) ->
 	after 5000 ->
 		error(timeout)
 	end.
+
+killed_streams_http(_) ->
+	doc("Ensure completed responses with a connection: close are not considered killed streams."),
+	{ok, _, OriginPort} = init_origin(tcp, http,
+		fun (_, ClientSocket, ClientTransport) ->
+			{ok, _} = ClientTransport:recv(ClientSocket, 0, 1000),
+			ClientTransport:send(ClientSocket,
+				"HTTP/1.1 200 OK\r\n"
+				"connection: close\r\n"
+				"content-length: 12\r\n"
+				"\r\n"
+				"hello world!"
+			)
+		end),
+	{ok, ConnPid} = gun:open("localhost", OriginPort),
+	{ok, http} = gun:await_up(ConnPid),
+	StreamRef = gun:get(ConnPid, "/"),
+	{response, nofin, 200, _} = gun:await(ConnPid, StreamRef),
+	{ok, <<"hello world!">>} = gun:await_body(ConnPid, StreamRef),
+	receive
+		{gun_down, ConnPid, http, normal, KilledStreams, _} ->
+			[] = KilledStreams,
+			gun:close(ConnPid)
+	after 1000 ->
+		error(timeout)
+	end.
+
+list_header_name(_) ->
+	doc("Header names may be given as list."),
+	{ok, OriginPid, OriginPort} = init_origin(tcp, http),
+	{ok, Pid} = gun:open("localhost", OriginPort),
+	{ok, http} = gun:await_up(Pid),
+	_ = gun:get(Pid, "/", [
+		{"User-Agent", "Gun/list-headers"}
+	]),
+	Data = receive_from(OriginPid),
+	Lines = binary:split(Data, <<"\r\n">>, [global]),
+	[<<"user-agent: Gun/list-headers">>] = [L || <<"user-agent: ", _/bits>> = L <- Lines],
+	gun:close(Pid).
+
+map_headers(_) ->
+	doc("Header names may be given as a map."),
+	{ok, OriginPid, OriginPort} = init_origin(tcp, http),
+	{ok, Pid} = gun:open("localhost", OriginPort),
+	{ok, http} = gun:await_up(Pid),
+	_ = gun:get(Pid, "/", #{
+		<<"USER-agent">> => "Gun/map-headers"
+	}),
+	Data = receive_from(OriginPid),
+	Lines = binary:split(Data, <<"\r\n">>, [global]),
+	[<<"user-agent: Gun/map-headers">>] = [L || <<"user-agent: ", _/bits>> = L <- Lines],
+	gun:close(Pid).
 
 reply_to(_) ->
 	doc("The reply_to option allows using a separate process for requests."),
@@ -200,7 +306,7 @@ do_reply_to(Protocol) ->
 	ReplyTo ! Ref,
 	receive
 		{response, _, _, _} ->
-			ok
+			gun:close(Pid)
 	after 1000 ->
 		error(timeout)
 	end.
@@ -227,6 +333,22 @@ retry_1(_) ->
 		error(timeout)
 	end.
 
+retry_immediately(_) ->
+	doc("Ensure Gun retries immediately."),
+	%% We have to make a first successful connection in order to test this.
+	{ok, _, OriginPort} = init_origin(tcp, http,
+		fun(_, ClientSocket, ClientTransport) ->
+			ClientTransport:close(ClientSocket)
+		end),
+	{ok, Pid} = gun:open("localhost", OriginPort, #{retry => 1, retry_timeout => 500}),
+	Ref = monitor(process, Pid),
+	receive
+		{'DOWN', Ref, process, Pid, {shutdown, _}} ->
+			ok
+	after 200 ->
+		error(timeout)
+	end.
+
 retry_timeout(_) ->
 	doc("Ensure the retry_timeout value is enforced."),
 	{ok, Pid} = gun:open("localhost", 12345, #{retry => 1, retry_timeout => 1000}),
@@ -240,8 +362,80 @@ retry_timeout(_) ->
 	receive
 		{'DOWN', Ref, process, Pid, {shutdown, _}} ->
 			ok
-	after 400 ->
+	after 800 ->
 		error(shutdown_too_late)
+	end.
+
+stream_info_http(_) ->
+	doc("Ensure the function gun:stream_info/2 works as expected for HTTP/1.1."),
+	{ok, _, OriginPort} = init_origin(tcp, http,
+		fun(_, ClientSocket, ClientTransport) ->
+			%% Give some time to detect the cancel.
+			timer:sleep(200),
+			%% Then terminate the stream.
+			ClientTransport:send(ClientSocket,
+				"HTTP/1.1 200 OK\r\n"
+				"content-length: 0\r\n"
+				"\r\n"
+			),
+			timer:sleep(400)
+		end),
+	{ok, Pid} = gun:open("localhost", OriginPort),
+	{ok, http} = gun:await_up(Pid),
+	{ok, undefined} = gun:stream_info(Pid, make_ref()),
+	StreamRef = gun:get(Pid, "/"),
+	Self = self(),
+	{ok, #{
+		ref := StreamRef,
+		reply_to := Self,
+		state := running
+	}} = gun:stream_info(Pid, StreamRef),
+	gun:cancel(Pid, StreamRef),
+	{ok, #{
+		ref := StreamRef,
+		reply_to := Self,
+		state := stopping
+	}} = gun:stream_info(Pid, StreamRef),
+	%% Wait a little for the stream to terminate.
+	timer:sleep(400),
+	{ok, undefined} = gun:stream_info(Pid, StreamRef),
+	%% Wait a little more for the connection to terminate.
+	timer:sleep(400),
+	{error, not_connected} = gun:stream_info(Pid, StreamRef),
+	gun:close(Pid).
+
+stream_info_http2(_) ->
+	doc("Ensure the function gun:stream_info/2 works as expected for HTTP/2."),
+	{ok, _, OriginPort} = init_origin(tcp, http2,
+		fun(_, _, _) -> timer:sleep(200) end),
+	{ok, Pid} = gun:open("localhost", OriginPort, #{
+		protocols => [http2]
+	}),
+	{ok, http2} = gun:await_up(Pid),
+	timer:sleep(100), %% Give enough time for the handshake to fully complete.
+	{ok, undefined} = gun:stream_info(Pid, make_ref()),
+	StreamRef = gun:get(Pid, "/"),
+	Self = self(),
+	{ok, #{
+		ref := StreamRef,
+		reply_to := Self,
+		state := running
+	}} = gun:stream_info(Pid, StreamRef),
+	gun:cancel(Pid, StreamRef),
+	%% Wait a little for the connection to terminate.
+	timer:sleep(300),
+	{error, not_connected} = gun:stream_info(Pid, StreamRef),
+	gun:close(Pid).
+
+shutdown_reason(_) ->
+	doc("The last connection failure must be propagated."),
+	{ok, Pid} = gun:open("localhost", 12345, #{retry => 0}),
+	Ref = monitor(process, Pid),
+	receive
+		{'DOWN', Ref, process, Pid, {shutdown, econnrefused}} ->
+			ok
+	after 200 ->
+		error(timeout)
 	end.
 
 transform_header_name(_) ->
@@ -260,10 +454,10 @@ transform_header_name(_) ->
 	{ok, Data} = gen_tcp:recv(ClientSocket, 0, 5000),
 	%% We do some very crude parsing of the response headers
 	%% to check that the header name was properly transformed.
-	Lines = binary:split(Data, <<"\r\n">>),
+	Lines = binary:split(Data, <<"\r\n">>, [global]),
 	HostLines = [L || <<"HOST: ", _/bits>> = L <- Lines],
 	1 = length(HostLines),
-	ok.
+	gun:close(Pid).
 
 unix_socket_connect(_) ->
 	case os:type() of
@@ -298,7 +492,20 @@ do_unix_socket_connect() ->
 	_ = gun:get(Pid, "/", [{<<"host">>, <<"localhost">>}]),
 	receive
 		{recv, _} ->
-			ok
+			gun:close(Pid)
 	after 250 ->
 		error(timeout)
 	end.
+
+uppercase_header_name(_) ->
+	doc("Header names may be given with uppercase characters."),
+	{ok, OriginPid, OriginPort} = init_origin(tcp, http),
+	{ok, Pid} = gun:open("localhost", OriginPort),
+	{ok, http} = gun:await_up(Pid),
+	_ = gun:get(Pid, "/", [
+		{<<"USER-agent">>, "Gun/uppercase-headers"}
+	]),
+	Data = receive_from(OriginPid),
+	Lines = binary:split(Data, <<"\r\n">>, [global]),
+	[<<"user-agent: Gun/uppercase-headers">>] = [L || <<"user-agent: ", _/bits>> = L <- Lines],
+	gun:close(Pid).
