057f4f9e511cf900a81ad02780e7d02452f96550
king22 <henrytan.next@gmail.com>
2020-05-08 12:04:44 +0700

    update unit-test

--- test/gun_test.exs
+++ test/gun_test.exs
@@ -29,7 +29,7 @@ defmodule GunTest do
 
   test "http_proxy" do
     # run
-    %{"proxy" => h, "proxy_user" => u, "proxy_password" => p} = Jason.decode! File.read!("./test/test_data.json")
+    %{"proxy" => h, "proxy_user" => u, "proxy_password" => p} = Jason.decode! File.read!("./test/lum_data.json")
     proxy = %{proxy: h, proxy_auth: {u, p}}
 #    IO.inspect proxy
     url = "http://lumtest.com/myip"
@@ -39,7 +39,28 @@ defmodule GunTest do
     # https
     url = "https://lumtest.com/myip"
     ret2 = Gun.http_request("GET", url, headers, "", opts, nil)
+    {_, hola_ip} = List.keyfind ret2.headers, "x-hola-ip", 0, {"x-hola-ip", "NONE"}
     assert(ret1.body == ret2.body)
+    assert(hola_ip == ret2.body)
+  end
+
+  test "rack_proxy" do
+    %{"proxy" => h} = Jason.decode! File.read!("./test/rack_data.json")
+    proxy = %{proxy: h}
+    url = "https://lumtest.com/myip"
+    headers = %{"connection" => "close"}
+    opts = Gun.default_option(25000) |> Map.merge(proxy) |> Map.merge(%{protocols: [:http]})
+    ret1 = Gun.http_request("GET", url, headers, "", opts, nil)
+#    IO.inspect ret1.headers
+    {_, hola_ip} = List.keyfind ret1.headers, "x-hola-ip", 0, {"x-hola-ip", "NONE"}
+    assert(hola_ip == ret1.body)
+
+    url = "https://lumtest.com/myip"
+    opts = Gun.default_option(25000) |> Map.merge(proxy)
+    ret2 = Gun.http_request("GET", url, headers, "", opts, nil)
+#    IO.inspect ret2.headers
+    {_, hola_ip} = List.keyfind ret2.headers, "x-hola-ip", 0, {"x-hola-ip", "NONE"}
+    assert(hola_ip == ret2.body)
   end
 
   test "error_code" do
